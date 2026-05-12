d7b3f5d0e7da7b9e7d587afbc75ab980be78597b
SouL29092005 <souvik.layek.300@gmail.com>
2026-05-12 21:04:02 +0530

    feat: company controller and routes

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,30 +1,29 @@
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
+  try {
+    const token = req.cookies.token;
 
-        if (!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false
-            });
-        }
-
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
-        req.id = decoded.userId;
-
-        next();
-
-    } catch (error) {
-        console.log(error);
-
-        return res.status(401).json({
-            message: "Invalid or expired token",
-            success: false
-        });
+    if (!token) {
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
+      });
     }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    req.id = decoded.userId;
+
+    next();
+  } catch (error) {
+    console.log(error);
+
+    return res.status(401).json({
+      message: "Invalid or expired token",
+      success: false,
+    });
+  }
 };
 
 export default isAuthenticated;
